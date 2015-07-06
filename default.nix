098c4e55ac1c0b3b73ebeb0ebe8487ca634b3956
lukasepple <git@lukasepple.de>
2015-07-06 20:27:01 +0200

    nix-build system.

---
+++ default.nix
@@ -0,0 +1,16 @@
+{ mkDerivation, base, bytestring, containers, directory, filepath
+, mtl, network, stdenv, text, transformers, unix, yaml
+}:
+mkDerivation {
+  pname = "spacecookie";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  buildDepends = [
+    base bytestring containers directory filepath mtl network text
+    transformers unix yaml
+  ];
+  description = "gopher server daemon";
+  license = stdenv.lib.licenses.gpl3;
+}
