dd5b24b2841dffa5b6be2d7f296f9ef478b7619b
sternenseemann <git@lukasepple.de>
2016-12-11 01:36:10 +0100

    Rework documentation of Network.Gopher.Util

--- src/Network/Gopher/Util.hs
+++ src/Network/Gopher/Util.hs
@@ -1,11 +1,21 @@
+{-|
+Module      : Network.Gopher.Util
+Stability   : experimental
+Portability : POSIX
+
+Helper utilities used within the library and the server which also could be useful for other application code.
+-}
 {-# LANGUAGE OverloadedStrings #-}
-module Network.Gopher.Util
-  ( stripNewline
-  , santinizePath
+module Network.Gopher.Util (
+  -- * Security
+    santinizePath
+  -- * String Encoding
   , asciiOrd
   , asciiChr
   , uEncode
   , uDecode
+  -- * Misc Helpers
+  , stripNewline
   ) where
 
 import Data.ByteString (ByteString ())
@@ -15,19 +25,23 @@ import qualified Data.String.UTF8 as U
 import Data.Word (Word8 ())
 import System.FilePath.Posix (pathSeparator, normalise, joinPath, splitPath)
 
+-- | 'chr' a 'Word8'
 asciiChr :: Word8 -> Char
 asciiChr = chr . fromIntegral
 
+-- | 'ord' a 'Word8'
 asciiOrd :: Char -> Word8
 asciiOrd = fromIntegral . ord
 
+-- | Encode a 'String' to a UTF-8 'ByteString'
 uEncode :: String -> ByteString
 uEncode = B.pack . U.encode
 
+-- | Decode a UTF-8 'ByteString' to a 'String'
 uDecode :: ByteString -> String
 uDecode = fst . U.decode . B.unpack
 
--- | Strip \r and \n from ByteStrings
+-- | Strip @\\r@ and @\\n@ from 'ByteString's
 stripNewline :: ByteString -> ByteString
 stripNewline s
   | B.null s           = B.empty
@@ -35,7 +49,6 @@ stripNewline s
     (map (fromIntegral . ord) "\n\r") = stripNewline (B.tail s)
   | otherwise          = B.head s `B.cons` stripNewline (B.tail s)
 
--- | Normalize the path and prevent path traversal
---   attacks.
+-- | Normalise a path and prevent <https://en.wikipedia.org/wiki/Directory_traversal_attack directory traversal attacks>.
 santinizePath :: FilePath -> FilePath
 santinizePath path = joinPath . filter (\p -> p /= ".." && p /= ".") . splitPath . normalise $ path
