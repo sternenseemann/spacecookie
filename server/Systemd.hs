e4e617f582cb8af25f52b7b29c17a7ca8432ae1e
sternenseemann <git@lukasepple.de>
2019-06-02 14:46:30 +0200

    Support for Fd-based systemd-activation
    This requires a possible future version of systemd,
    which is currently in PR.

--- server/Systemd.hs
+++ server/Systemd.hs
@@ -6,46 +6,54 @@ module Systemd
   , systemdStoreOrClose
   ) where
 
-import Control.Concurrent.MVar (newMVar, takeMVar)
+import Control.Concurrent.MVar (newMVar, takeMVar, mkWeakMVar)
 import Control.Exception.Base
+import Control.Monad (when, void)
+import Foreign.C.Types (CInt (..))
+import GHC.Conc (closeFdWith)
 import Network.Gopher (setupGopherSocket, GopherConfig (..))
-import qualified Network.Socket as S
 import System.Exit
+import System.Posix.Types (Fd (..))
 import System.Socket hiding (Error (..))
 import System.Socket.Family.Inet6
 import System.Socket.Type.Stream
 import System.Socket.Protocol.TCP
 import System.Socket.Unsafe (Socket (..))
-import System.Systemd.Daemon
+import System.Systemd.Daemon (notifyReady, notifyStopping)
+import System.Systemd.Daemon.Fd (storeFd, getActivatedSockets)
 
-data SystemdException = NoStream | IncorrectNum
+foreign import ccall unsafe "close"
+  c_close :: CInt -> IO CInt
+
+-- TODO Check Socket type, ...
+data SystemdException = IncorrectNum | InvalidFd
   deriving (Eq, Ord)
 
 instance Show SystemdException where
-  show NoStream = "SystemdException: Socket is not of type Stream"
   show IncorrectNum = "SystemdException: Only exactly one Socket is supported"
+  show InvalidFd    = "SystemdException: Invalid File Descriptor received"
 instance Exception SystemdException
 
 systemdSocket :: GopherConfig -> IO (Socket Inet6 Stream TCP)
-systemdSocket cfg = getSock
-  where sockConvert :: S.Socket -> IO (Socket Inet6 Stream TCP)
-        sockConvert s = S.fdSocket s >>= (fmap Socket . newMVar . fromIntegral)
-        getSock = getActivatedSockets >>= \sockets ->
-          case sockets of
-            Nothing -> setupGopherSocket cfg
-            Just [sock] -> do
-              t <- S.getSocketType sock
-              if t /= S.Stream
-                 then throwIO NoStream
-                 else sockConvert sock
-            Just l -> throwIO IncorrectNum
+systemdSocket cfg = getActivatedSockets >>= \sockets ->
+  case sockets of
+    Nothing -> setupGopherSocket cfg
+    Just [fd] -> toSocket fd
+    Just _ -> throwIO IncorrectNum
+  where toSocket :: Fd -> IO (Socket Inet6 Stream TCP)
+        toSocket fd = do
+          when (fd < 0) $ throwIO InvalidFd
+          mfd <- newMVar (fromIntegral fd)
+          let s = Socket mfd
+          _ <- mkWeakMVar mfd (close s)
+          pure s
 
 systemdStoreOrClose :: Socket Inet6 Stream TCP -> IO ()
 systemdStoreOrClose s = do
-  s' <- kcosConvert s
-  res <- storeFd s'
+  fd <- toFd s
+  res <- storeFd fd
   case res of
     Just () -> return ()
-    Nothing -> S.close s'
-  where kcosConvert :: Socket Inet6 Stream TCP -> IO S.Socket
-        kcosConvert (Socket mvar) = fmap fromIntegral (takeMVar mvar) >>= S.mkSocket
+    Nothing -> closeFdWith (void . c_close . fromIntegral) fd
+  where toFd :: Socket Inet6 Stream TCP -> IO Fd
+        toFd (Socket mvar) = fmap (Fd . fromIntegral) (takeMVar mvar)
