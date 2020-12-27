e6b96e0c44bf3bfb83c22beac29e1f5ea4bcf82f
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2020-12-27 21:37:38 +0100

    feat(server): make log format configurable
    add new "log" config entry with the following sub-options:
    
    * enable: whether to log
    * hide-ips: whether to hide ips in log output
    * hide-time: whether to disable timestamps in log output
    * level: log level ("info" or "error")
    
    This is used to set the appropriate GopheLogConfig.

--- server/Config.hs
+++ server/Config.hs
@@ -1,40 +1,65 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Config
   ( Config (..)
+  , LogConfig (..)
   ) where
 
 import Control.Monad (mzero, join)
 import Control.Applicative ((<|>))
 import Data.Aeson
 import Data.ByteString (ByteString ())
+import Data.Text (toLower)
+import Network.Gopher.Log (LogLevel (..))
 import Network.Gopher.Util
 
-data Config = Config { serverName    :: ByteString
-                     , listenAddr    :: Maybe ByteString
-                     , serverPort    :: Integer
-                     , runUserName   :: Maybe String
-                     , rootDirectory :: FilePath
-                     }
+data Config
+  = Config
+  { serverName    :: ByteString
+  , listenAddr    :: Maybe ByteString
+  , serverPort    :: Integer
+  , runUserName   :: Maybe String
+  , rootDirectory :: FilePath
+  , logConfig     :: LogConfig
+  }
 
 instance FromJSON Config where
-  parseJSON (Object v) = Config <$>
-    v .: "hostname" <*>
-    (v .:? "listen" >>= fmap join . traverse (.:? "addr")) <*>
-    ((v .: "listen" >>= (.: "port")) <|> v .:? "port" .!= 70) <*>
-    v .:? "user" <*>
-    v .: "root"
+  parseJSON (Object v) = Config
+    <$> v .: "hostname"
+    <*> (v .:? "listen" >>= fmap join . traverse (.:? "addr"))
+    <*> ((v .: "listen" >>= (.: "port")) <|> v .:? "port" .!= 70)
+    <*> v .:? "user"
+    <*> v .: "root"
+    <*> v .:? "log" .!= defaultLogConfig
   parseJSON _ = mzero
 
-instance ToJSON Config where
-  toJSON (Config host addr port user root) = object $
-    [ "hostname" .= host
-    , "listen" .= listenObj
-    , "root" .= root
-    ] ++ maybeBind "user" user
-    where listenObj = object $ maybeBind "addr" addr ++ [ "port" .= port ]
-          maybeBind n v = maybe [] ((:[]) . (n .=)) v
+data LogConfig
+  = LogConfig
+  { logEnable   :: Bool
+  , logHideIps  :: Bool
+  , logHideTime :: Bool
+  , logLevel    :: LogLevel
+  }
+
+defaultLogConfig :: LogConfig
+defaultLogConfig = LogConfig True True False LogLevelInfo
+
+instance FromJSON LogConfig where
+  parseJSON (Object v) = LogConfig
+    <$> v .:  "enable"
+    <*> v .:? "hide-ips"  .!= logHideIps defaultLogConfig
+    <*> v .:? "hide-time" .!= logHideTime defaultLogConfig
+    <*> v .:? "level"     .!= logLevel defaultLogConfig
+  parseJSON _ = mzero
 
 -- auxiliary instances for types that have no default instance
+instance FromJSON LogLevel where
+  parseJSON (String s) =
+    case toLower s of
+      "info"  -> pure LogLevelInfo
+      "error" -> pure LogLevelError
+      _ -> mzero
+  parseJSON _ = mzero
+
 instance FromJSON ByteString where
   parseJSON s@(String _) = uEncode <$> parseJSON s
   parseJSON _ = mzero
